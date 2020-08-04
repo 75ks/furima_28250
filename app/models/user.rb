54fde17404f1daef8740d60aea6a6f0ce69f59a6
75ks <kenta.s705@gmail.com>
2020-08-04 20:07:15 +0900

    バリデーション設定＆name属性設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :email, uniqueness: true
+
+  with_options presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, message: '全角文字を使用してください'} do
+    validates :family_name
+    validates :first_name
+    validates :family_name_kana
+    validates :first_name_kana
+  end
+  
+  with_options presence: true do
+    validates :name
+    validates :family_name
+    validates :first_name
+    validates :family_name_kana
+    validates :first_name_kana
+    validates :birthday
+  end
 end
