b28cff995f5d18ca0437e85f00d2e7c6df9089d1
75ks <kenta.s705@gmail.com>
2020-08-05 23:16:57 +0900

    正規表現を変数に代入

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,14 +6,17 @@ class User < ApplicationRecord
 
   validates :email, uniqueness: true
 
-  validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i, message: '半角英数字を使用してください' }
+  half_width_alphanumeric = '\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z'
+  validates :password, presence: true, format: { with: /#{half_width_alphanumeric}/i, message: '半角英数字を使用してください' }
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: '全角文字を使用してください' } do
+  full_width_character = '\A[ぁ-んァ-ン一-龥]'
+  with_options presence: true, format: { with: /#{full_width_character}/, message: '全角文字を使用してください' } do
     validates :family_name
     validates :first_name
   end
 
-  with_options presence: true, format: { with: /\A[ァ-ンー－]/, message: '全角カタカナを使用してください' } do
+  full_width_kana = '\A[ァ-ンー－]'
+  with_options presence: true, format: { with: /#{full_width_kana}/, message: '全角カタカナを使用してください' } do
     validates :family_name_kana
     validates :first_name_kana
   end
