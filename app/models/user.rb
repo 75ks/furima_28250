6dc64a3ccb32d3b9d98783e6e9f4805ecabc311c
75ks <kenta.s705@gmail.com>
2020-08-06 12:01:53 +0900

    正規表現の変数のクォーテーションを削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,17 +6,17 @@ class User < ApplicationRecord
 
   validates :email, uniqueness: true
 
-  half_width_alphanumeric = '\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z'
-  validates :password, presence: true, format: { with: /#{half_width_alphanumeric}/i, message: '半角英数字を使用してください' }
+  half_width_alphanumeric = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i
+  validates :password, presence: true, format: { with: half_width_alphanumeric, message: '半角英数字を使用してください' }
 
-  full_width_character = '\A[ぁ-んァ-ン一-龥]'
-  with_options presence: true, format: { with: /#{full_width_character}/, message: '全角文字を使用してください' } do
+  full_width_character = /\A[ぁ-んァ-ン一-龥]/
+  with_options presence: true, format: { with: full_width_character, message: '全角文字を使用してください' } do
     validates :family_name
     validates :first_name
   end
 
-  full_width_kana = '\A[ァ-ンー－]'
-  with_options presence: true, format: { with: /#{full_width_kana}/, message: '全角カタカナを使用してください' } do
+  full_width_kana = /\A[ァ-ンー－]/
+  with_options presence: true, format: { with: full_width_kana, message: '全角カタカナを使用してください' } do
     validates :family_name_kana
     validates :first_name_kana
   end
