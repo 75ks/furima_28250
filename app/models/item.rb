9d34be1ed3674a74f0587b9787ff30922329e52e
75ks <kenta.s705@gmail.com>
2020-08-06 18:04:39 +0900

    itemモデル、itemsテーブルの作成とアソシエーション設定

---
+++ app/models/item.rb
@@ -0,0 +1,30 @@
+class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :item_condition
+  belongs_to_active_hash :postage_payer
+  belongs_to_active_hash :area
+  belongs_to_active_hash :preparation_day
+  belongs_to             :user
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :item_condition_id
+    validates :postage_payer_id
+    validates :area_id
+    validates :preparation_day_id
+  end
+
+  with_options presence: true do
+    validates :name
+    validates :image
+    validates :price
+    validates :introduction
+    validates :category_id
+    validates :item_condition_id
+    validates :postage_payer_id
+    validates :area_id
+    validates :preparation_day_id
+    validates :user
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_04_085855) do
+ActiveRecord::Schema.define(version: 2020_08_06_064034) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "image", null: false
+    t.integer "price", null: false
+    t.text "introduction", null: false
+    t.integer "category_id", null: false
+    t.integer "item_condition_id", null: false
+    t.integer "postage_payer_id", null: false
+    t.integer "area_id", null: false
+    t.integer "preparation_day_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,4 +46,5 @@ ActiveRecord::Schema.define(version: 2020_08_04_085855) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
