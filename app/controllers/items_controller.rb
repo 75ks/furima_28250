e511ec5cbb0e11e3ff45ed94bde56cc934f2c1d8
75ks <kenta.s705@gmail.com>
2020-08-07 15:52:27 +0900

    new、createアクションの処理＆ストロングパラメーターの設定、処理

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,10 +4,15 @@ class ItemsController < ApplicationController
   end
   
   def new
-
+    @item = Item.new
   end
 
   def create
-    
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :image, :price, :introduction, :category_id, :item_condition_id, :postage_payer_id, :area_id, :preparation_day_id).merge(user_id: current_user.id)
   end
 end
