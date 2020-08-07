ca59b447b8b76d2d2b57d833662c240d7ca7d1ff
75ks <kenta.s705@gmail.com>
2020-08-07 12:37:31 +0900

    Active Storageの導入とアソシエーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,6 +6,7 @@ class Item < ApplicationRecord
   belongs_to_active_hash :area
   belongs_to_active_hash :preparation_day
   belongs_to             :user
+  has_one_attached       :image
 
   with_options numericality: { other_than: 1 } do
     validates :category_id
