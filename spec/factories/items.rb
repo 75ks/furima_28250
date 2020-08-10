694b38ce98c45b607f76ab0fb619340b3b059ae6
75ks <kenta.s705@gmail.com>
2020-08-10 11:24:00 +0900

    FactoryBotの作成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,4 +1,12 @@
 FactoryBot.define do
   factory :item do
+    name               {Faker::Food.fruits}
+    price              {Faker::Number.within(range: 300..9_999_999)}
+    introduction       {Faker::Food.fruits}
+    category_id        {Faker::Number.within(range: 2..11)}
+    item_condition_id  {Faker::Number.within(range: 2..7)}
+    postage_payer_id   {Faker::Number.within(range: 2..3)}
+    area_id            {Faker::Number.within(range: 2..48)}
+    preparation_day_id {Faker::Number.within(range: 2..4)}
   end
 end
