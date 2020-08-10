f12e82dc930a61cf228b6994cd5f4c709d8f8561
75ks <kenta.s705@gmail.com>
2020-08-10 16:34:51 +0900

    FactoryBotの修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :item do
-    name               { Faker::Food.fruits }
-    price              { Faker::Number.within(range: 300..9_999_999) }
-    introduction       { Faker::Food.fruits }
-    category_id        { Faker::Number.within(range: 2..11) }
-    item_condition_id  { Faker::Number.within(range: 2..7) }
-    postage_payer_id   { Faker::Number.within(range: 2..3) }
-    area_id            { Faker::Number.within(range: 2..48) }
-    preparation_day_id { Faker::Number.within(range: 2..4) }
+    name               { "Tシャツ" }
+    price              { 3000 }
+    introduction       { "Tシャツです。色は白です。" }
+    category_id        { 3 }
+    item_condition_id  { 2 }
+    postage_payer_id   { 3 }
+    area_id            { 12 }
+    preparation_day_id { 2 }
     association        :user
   end
 end
