a7cae4cf97c12438a966cd8213f3d5056e5b11cf
75ks <kenta.s705@gmail.com>
2020-08-10 19:36:26 +0900

    エラー文の表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,8 +10,9 @@ class ItemsController < ApplicationController
   end
 
   def create
-    item = Item.new(item_params)
-    if item.save
+   
+    @item = Item.new(item_params)
+    if @item.save
       redirect_to root_path
     else
       render :new
