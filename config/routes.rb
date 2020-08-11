0dc4812e330411b80ac9ccc63987275979f2a875
75ks <kenta.s705@gmail.com>
2020-08-11 19:44:17 +0900

    Revert "トップページ削除"
    This reverts commit 64bf9fa961a4ecc9b96bace2be52372bf6893c9e.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  
-  resources :items, only: [:new, :create, :show]
+  root 'items#index'
+  resources :items, only: [:index, :new, :create, :show]
 end
