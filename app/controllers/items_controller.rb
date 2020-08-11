64bf9fa961a4ecc9b96bace2be52372bf6893c9e
75ks <kenta.s705@gmail.com>
2020-08-11 19:42:49 +0900

    トップページ削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,9 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: :new
   before_action :set_item, only: :show
 
-  def index
-    @items = Item.order('created_at DESC')
-  end
+  
 
   def new
     @item = Item.new
