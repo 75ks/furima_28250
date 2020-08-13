d2c0f819e951f7ec65aafb73aa7edb408b17ee21
75ks <kenta.s705@gmail.com>
2020-08-13 14:27:02 +0900

    出品者と購入済商品が購入ページに遷移するURL直打ち対策

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,11 +1,22 @@
 class TransactionsController < ApplicationController
   before_action :authenticate_user!, only: :index
-
+  before_action :correct_user, only: :index
   def index
 
   end
 
   def create
 
+  end
+
+  private
+
+  def correct_user
+    item = Item.find(params[:id])
+    if user_signed_in? && current_user.id == item.user_id
+      redirect_to root_path
+    elsif user_signed_in? && Order.find_by(item_id: item.id).present?
+      redirect_to root_path
+    end
   end
 end
