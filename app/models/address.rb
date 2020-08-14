f1595c4761ebc34b1ea702187de60c46542b3c97
75ks <kenta.s705@gmail.com>
2020-08-14 14:22:10 +0900

    郵便番号にハイフンありと電話番号にハイフンなしで11桁以内のバリデーション

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,6 +2,12 @@ class Address < ApplicationRecord
   belongs_to_active_hash :area
   belongs_to :item
 
+  post_code = /\A\d{3}[-]\d{4}\z/
+  validates :post_code, presence:true, format: { with: post_code, message: 'ハイフンを使用してください'}
+
+  phone_number = /\A\d{1, 11}\z/
+  validates :phone_number, presence:true, format: { with: phone_number, message: '11桁以内でハイフンは使用しないでください'}
+
   with_options presence: true do
     validates :post_code
     validates :area_id
