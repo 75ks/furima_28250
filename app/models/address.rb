052a43ddc83af85fa56ad4c5b49fa4ca8ca2e9e9
75ks <kenta.s705@gmail.com>
2020-08-14 12:26:50 +0900

    addressesテーブルの必須項目のバリデーション設定

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,12 @@
 class Address < ApplicationRecord
   belongs_to :item
+
+  with_options presence: true do
+    validates :post_code
+    validates :area_id
+    validates :city
+    validates :house_number
+    validates :phone_number
+    validates :item
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 2020_08_14_025626) do
     t.integer "area_id", null: false
     t.string "city", null: false
     t.string "house_number", null: false
-    t.string "building_name", null: false
+    t.string "building_name"
     t.string "phone_number", null: false
     t.bigint "item_id", null: false
     t.datetime "created_at", precision: 6, null: false
