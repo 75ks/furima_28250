bcc8a083b83ab8b7b6402aef38e3ffddc20bbd98
75ks <kenta.s705@gmail.com>
2020-08-15 16:52:40 +0900

    createアクションの編集＆支払い情報の生成

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -6,7 +6,16 @@ class TransactionsController < ApplicationController
   end
 
   def create
-    @order = Order.new(user_id: order_params[:user_id], item_id: order_params[:item_id])
+    @item = Item.find(params[:id])
+    @order = OrderAddress.new(order_params)
+    
+    if @order.valid?
+      pay_item
+      @order.save
+      return redirect_to root_path
+    else
+      render :index
+    end
   end
 
   private
@@ -21,6 +30,15 @@ class TransactionsController < ApplicationController
   end
 
   def order_params
-    params.permit(:token).merge(user_id: current_user.id, item_id: @item.id)
+    params.permit(:token, :post_code, :area_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
   end
 end
