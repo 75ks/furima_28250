6d3fcc6d33628090112e0c218429b731fe7fec24
75ks <kenta.s705@gmail.com>
2020-08-15 14:31:42 +0900

    トークン生成の処理の編集2

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -8,12 +8,11 @@ const pay = function() {
     const formData = new FormData(formResult);
 
     const card = {
-      number: formData.get("card-number"),
+      number: formData.get("number"),
       cvc: formData.get("cvc"),
-      exp_month: formData.get("card-exp-month"),
-      exp_year: `20${formData.get("card-exp-year")}`,
+      exp_month: formData.get("exp_month"),
+      exp_year: `20${formData.get("exp_year")}`,
     };
-
     Payjp.createToken(card, function(status, response) {
       if (status == 200) {
         const token = response.id;
@@ -22,13 +21,14 @@ const pay = function() {
         renderDom.insertAdjacentHTML("beforeend", tokenObj);
 
         document.getElementById("card-number").removeAttribute("name");
-        document.getElementById("cvc").removeAttribute("name");
+        document.getElementById("card-cvc").removeAttribute("name");
         document.getElementById("card-exp-month").removeAttribute("name");
         document.getElementById("card-exp-year").removeAttribute("name");
 
         document.getElementById("charge-form").submit();
         document.getElementById("charge-form").reset();
       } else {
+        alert("error")
       }
     });
   });
