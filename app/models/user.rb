62bae93f3006cccb86e18392abd5421d152da221
75ks <kenta.s705@gmail.com>
2020-08-17 19:09:58 +0900

    dependent: :destroyの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :items, foreign_key: :user_id, dependent: :destroy
-  has_many :orders, foreign_key: :user_id, dependent: :destroy
+  has_many :items, dependent: :destroy
+  has_many :orders, dependent: :destroy
 
   validates :email, uniqueness: true
 
