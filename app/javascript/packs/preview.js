40e26aea45e3654e095fd4505ca66fe657375243
75ks <kenta.s705@gmail.com>
2020-08-18 22:01:36 +0900

    画像のプレビュー機能の実装

---
+++ app/javascript/packs/preview.js
@@ -0,0 +1,29 @@
+if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {  
+  document.addEventListener('DOMContentLoaded', function(){
+    const ImageList = document.getElementById('image-list')
+
+    const createImageHTML = (blob) => {
+      const imageElement = document.createElement('div')
+      const blobImage = document.createElement('img')
+      blobImage.setAttribute('id', 'image-list-child')
+      blobImage.setAttribute('src', blob)
+      imageElement.appendChild(blobImage)
+      ImageList.appendChild(imageElement)
+    }
+
+    document.getElementById('item-image').addEventListener('change', function(e){
+      const imageContent = document.querySelector('#image-list-child');
+      const editContent = document.querySelector('#edit-image')
+      if (editContent){
+        editContent.remove();
+      } else if (imageContent){
+        imageContent.remove();
+      };
+      
+      const file = e.target.files[0];
+      const blob = window.URL.createObjectURL(file);
+
+      createImageHTML(blob)
+    });
+  });
+} 
