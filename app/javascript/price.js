3cd0260012b33a5496efd26f9b9adbe2b0ef8f24
75ks <kenta.s705@gmail.com>
2020-08-19 00:13:17 +0900

    jsを指定のURLでのみ読み込む

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -1,12 +1,14 @@
-window.addEventListener('load', function(){
-  let input = document.getElementById("item-price");
-  let tax = document.getElementById("add-tax-price");
-  let profit = document.getElementById("profit");
-  input.addEventListener('keyup', function(){
-    let input_price = document.getElementById("item-price").value;
-    taxTest = input_price * 0.1;
-    profitTest = input_price - (input_price * 0.1);
-    tax.innerHTML = taxTest;
-    profit.innerHTML = profitTest;
+if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
+  window.addEventListener('load', function(){
+    let input = document.getElementById("item-price");
+    let tax = document.getElementById("add-tax-price");
+    let profit = document.getElementById("profit");
+    input.addEventListener('keyup', function(){
+      let input_price = document.getElementById("item-price").value;
+      taxTest = input_price * 0.1;
+      profitTest = input_price - (input_price * 0.1);
+      tax.innerHTML = taxTest;
+      profit.innerHTML = profitTest;
+    });
   });
-});
+}
