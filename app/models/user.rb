738824ab9497de5ceeec88632c35caa333b3b54d
75ks <kenta.s705@gmail.com>
2020-08-20 11:04:29 +0900

    messageモデル、messagesテーブルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
 
   has_many :items, dependent: :destroy
   has_many :orders, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
   validates :email, uniqueness: true
 
