edf7314842ae503c20e10fbd5003b7387ac0585c
75ks <kenta.s705@gmail.com>
2020-08-21 16:54:50 +0900

    更新

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks',
     registrations: 'users/registrations'
-  }
+  } 
   root 'items#index'
   resources :items, only: [:new, :create, :show, :edit, :update, :destroy] do
     member do
